912c736df109bb663cf394267c8e8568ee5f72e3
Diego Kaleb Valenzuela Carrillo <a01018992@itesm.mx>
2020-03-07 21:55:50 -0800

    Board Design

---
+++ Labyrinth-VR/Assets/Scripts/BoardControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardControl : MonoBehaviour
+{
+    public Vector3 _CurrentRotation;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _CurrentRotation = GetComponent<Transform>().eulerAngles;
+
+        // Cases:
+        if((Input.GetKey(KeyCode.W)) && (_CurrentRotation.z <= 10 || _CurrentRotation.z >= 348))
+        {
+            transform.Rotate(1, 0, 0);
+        }
+
+        if ((Input.GetKey(KeyCode.S)) && (_CurrentRotation.z >= 349 || _CurrentRotation.z <= 11))
+        {
+            transform.Rotate(-1, 0, 0);
+        }
+    }
+}
